fc3ac7bc99c269016e2094812d80ec3a173f736c
Kelly Mersereau <kmerseraeu@SENMBP51s-MacBook-Pro.local>
2017-01-24 16:38:51 -0500

    updates

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,14 +1,87 @@
-var Twitter = require('twitter');
+// Initialize the app
+var express = require('express');
+var app     = express();
 
-var client = new Twitter({
-  consumer_key: 'i5Ggp9W7LclN94Wv2IXP7FqnS',
-  consumer_secret: 'ugpwVDFkIpXTG3bB8d1h9e5b1YxcgCzmX8B7Gi6PS9DX1MLXWo',
-  access_token_key: '	23433346-T6ZURW2thjBVx9w6oacNg7IPboPGuRugZToMUbeca',
-  access_token_secret: 'ntIKosmnTM6mHsVZn0vkcTumtvkxsJVwnnZy5mGYHqUyO'
+
+// Initialize the Twitter client
+var Twitter = require('twitter');
+var config  = require('./config.js');
+var twitter = new Twitter(config);
+
+// Get the user IDs of 100 friends
+function getTweets() {
+    twitter.get('search/tweets', { q: '#WhyIMarch', count: 100 }, function(err, data) {
+
+        // If we have the IDs, we can look up user information
+        if (!err && data) {
+            console.log(data);
+        }
+
+        // Otherwise, return with error
+        else {
+            next(err);
+        }
+    });
+}
+
+// Get user information for the array of user IDs provided
+function lookupUsers(user_ids, next) {
+    twitter.get('users/lookup', { user_id: user_ids.join() }, function(err, data) {
+
+        // If we have user information, we can pass it along to render
+        if (!err && data) {
+
+            // We'll fill this array with the friend data you need
+            var friends_array = new Array();
+
+            for (index in data) {
+
+                // Get your friend's join date and do some leading zero magic
+                var date = new Date(data[index].created_at);
+                var date_str = date.getFullYear() + '-'
+                             + ('0' + (date.getMonth()+1)).slice(-2) + '-'
+                             + ('0' + date.getDate()).slice(-2);
+
+                // Push the info to an array
+                friends_array.push({
+                    'name'          : data[index].name,
+                    'screen_name'   : data[index].screen_name,
+                    'created_at'    : date_str,
+                    'profile_image' : data[index].profile_image_url,
+                    'link_color'    : data[index].profile_link_color
+                });
+            }
+
+            // The callback function defined in the getFriends call
+            next(err, friends_array);
+        }
+
+        // Otherwise, return with error
+        else {
+            next(err);
+        }
+    });
+}
+
+// This is the route for our index page
+app.get('/', function(req, res){
+
+    // Calling the function defined above to get friend information
+    getFriends(function(err, data) {
+
+        // Render the page with our Twitter data
+        if (!err && data) {
+            res.render('index', { friends: data });
+        }
+
+        // Otherwise, render an error page
+        else {
+            res.send('Something went wrong :(\n'+err.message);
+        }
+    });
 });
 
-
-client.get('search/tweets', {q: '#cats'}, function(error, tweets, response) {
-   console.log(tweets);
-   console.log(response);
+// Start the server
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
 });
---
+++ src/javascript/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'i5Ggp9W7LclN94Wv2IXP7FqnS',
+	consumer_secret:'ugpwVDFkIpXTG3bB8d1h9e5b1YxcgCzmX8B7Gi6PS9DX1MLXWo',
+	access_token: '23433346-T6ZURW2thjBVx9w6oacNg7IPboPGuRugZToMUbeca',
+	access_token_secret: 'ntIKosmnTM6mHsVZn0vkcTumtvkxsJVwnnZy5mGYHqUyO',
+};
